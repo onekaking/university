21616bd20511148bcf12b461437f679734ffc2d9
thienhoang <thienhoang@pros-MacBook-Pro.local>
2016-05-23 21:33:00 +0700

    Init Model

---
+++ api/controllers/ImageController.js
@@ -0,0 +1,11 @@
+/**
+ * ImageController
+ *
+ * @description :: Server-side logic for managing Images
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
